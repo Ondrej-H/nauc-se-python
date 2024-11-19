4a46f7ad8b023bf84cf3d9bd1c3c87558e6e7bd3
Ondřej Hubert <ondrej.hubert@gmail.com>
2024-11-19 20:33:12 +0100

    add lesson 02

---
+++ 02/printing_errors.py
@@ -0,0 +1,8 @@
+
+#print(V míse je maso)          # SyntaxError
+#print("V míse je maso" + 3)    # TypeError
+#print(5 / 0)                   # ZeroDivisionError
+#print(jmeno)                   # NameError
+#print(len + 1)                 # TypeError
+#print(int('blablabla'))        # ValueError
+#print(int('8.9'))              # ValueError
