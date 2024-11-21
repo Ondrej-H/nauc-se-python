a84fac770df34414d69702977347ff9f23e7cde0
Ondřej Hubert <ondrej.hubert@gmail.com>
2024-11-21 15:42:05 +0100

    První krok
    Do vytvořeného modulu zkousim_git.py jsem přidal další funkci,
    abych viděl změny, až za malou chvíli použiju git diff.
    Pokračování: Zkusme vytvořit první větev!

--- 09/zkousim_git.py
+++ 09/zkousim_git.py
@@ -1,4 +1,29 @@
 
 def vypis_ucel_tohoto_souboru():
+    """
+    Tato funkce vrací jen výpis.
+    Existuje pouze proto, abych viděl změny v Gitu.
+    """
     return "Tento soubor slouží k procvičování Gitu."
 
+
+def popis_krok_1():
+    """
+    Popíše, jak proběhl můj první krok.
+    """
+    return
+"""
+V prvním kroku jsem vytvořil modul zkousim_git.py
+a jeho spouštěcí program zkousim_git_spoustec.py.
+Modul zkousim_git.py obsahoval pouze jedinou funkci
+vypis_ucel_tohoto_souboru().
+Nyní přidávám funkci druhou, která tímto popisuje,
+co jsem udělal v prvním kroku.
+Pokračování: commitnu tyto změny a vyzkouším git diff.
+"""
+
+
+def popis_krok_2():
+    """
+    Zde si předpřipravuji funkci pro druhý krok.
+    """
