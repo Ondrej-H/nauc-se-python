dfd392a892d5c5b6a6c0932bce7e14b3d37c55b4
Ondřej Hubert <ondrej.hubert@gmail.com>
2024-11-21 15:01:25 +0100

    Procvičování Gitu
    Vytvořil jsem primitivní modul zkousim_git.py
    a jeho spousteci program zkousim_git_spoustec.
    Prozatím zkouším Git v powershellu.
    Pokračování: Udělat v modulu změny, vyzkoušet git diff.

---
+++ 09/zkousim_git.py
@@ -0,0 +1,4 @@
+
+def vypis_ucel_tohoto_souboru():
+    return "Tento soubor slouží k procvičování Gitu."
+
---
+++ 09/zkousim_git_spoustec.py
@@ -0,0 +1,4 @@
+
+from zkousim_git import vypis_ucel_tohoto_souboru
+
+print(vypis_ucel_tohoto_souboru())
