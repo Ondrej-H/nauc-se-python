3b132c6809d22ac31894c94b6e0d017b1e9e9ca5
Ondřej Hubert <ondrej.hubert@gmail.com>
2024-11-22 15:44:59 +0100

    add lesson 09

--- 09/zkousim_git.py
+++ 09/zkousim_git.py
@@ -24,10 +24,22 @@ Pokračování: commitnu tyto změny a vyzkouším git diff.
 
 def popis_krok_2():
     """
-    Zde si předpřipravuji funkci pro druhý krok.
+    Popíše jak proběhl můj druhý krok.
     """
     return """
 Zjistil jsem, že git diff ukáže rozdíl jen když jsem provedl změny,
 ale ještě jsem je necommitnul. Takže teď sem jen připíšu toto,
 nebudu commitovat a zadám git diff, ať to konečně ozkouším.
+"""
+
+
+def popis_krok_3():
+    """
+    Popíše můj třetí krok.
+    """
+    return """
+git diff už jsem vyzkoušel a jdu "větvit". Pomocí git branch uceni_vetveni
+jsem vytvořil novou větev uceni_vetveni a pomocí git checkout uceni_vetveni
+jsem na ni přepnul. Teď comitnu tuto změnu (vytvoření této funkce) a také
+funkci přidám do spouštěče a comitnu.
 """
--- 09/zkousim_git_spoustec.py
+++ 09/zkousim_git_spoustec.py
@@ -6,3 +6,5 @@ print(zkousim_git.vypis_ucel_tohoto_souboru())
 print(zkousim_git.popis_krok_1())
 
 print(zkousim_git.popis_krok_2())
+
+print(zkousim_git.popis_krok_3())
